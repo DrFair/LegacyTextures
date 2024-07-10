d9f7f843e91b7f42452246b05d15f339cd377d6e
Mads Skovgaard <madsskovgaard@hotmail.com>
2024-07-10 11:48:55 +0200

    Adds HumanDrawOptionsPatch to remove facial hair etc.

---
+++ src/main/java/legacytextures/patches/HumanDrawOptionsPatch.java
@@ -0,0 +1,17 @@
+package legacytextures.patches;
+
+import necesse.engine.modLoader.annotations.ModMethodPatch;
+import necesse.gfx.drawOptions.human.HumanDrawOptions;
+import net.bytebuddy.asm.Advice;
+
+@ModMethodPatch(target = HumanDrawOptions.class, name = "pos", arguments = {int.class, int.class})
+public class HumanDrawOptionsPatch {
+
+    @Advice.OnMethodEnter()
+    static void onEnter(@Advice.This HumanDrawOptions drawOptions) {
+        drawOptions.eyelidsTexture(null);
+        drawOptions.facialFeatureTexture(null);
+        drawOptions.backFacialFeatureTexture(null);
+        drawOptions.blinking(false);
+    }
+}
